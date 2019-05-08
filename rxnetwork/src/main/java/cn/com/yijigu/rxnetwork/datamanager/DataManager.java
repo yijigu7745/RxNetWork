3c7855bde8023e670cd2351d3dbff797151a6648
fuzhoughinfo <ghinfo@gh-hitech.com>
2019-05-08 14:59:48 +0800

    RxNetWork

--- rxnetwork/src/main/java/cn/com/yijigu/rxnetwork/datamanager/DataManager.java
+++ rxnetwork/src/main/java/cn/com/yijigu/rxnetwork/datamanager/DataManager.java
@@ -13,20 +13,20 @@ public class DataManager {
     private static final String COOKIE = "cookie";
 
     public static String getCookie() {
-        SharedPreferences preferences = RetrofitUtilsApplication.getAppContext().getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        SharedPreferences preferences = RetrofitUtilsApplication.getRetrofitUtilsContext().getSharedPreferences(NAME, Context.MODE_PRIVATE);
         return preferences.getString(COOKIE, "");
     }
     public static void putCookie( String value) {
-        SharedPreferences preferences = RetrofitUtilsApplication.getAppContext().getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        SharedPreferences preferences = RetrofitUtilsApplication.getRetrofitUtilsContext().getSharedPreferences(NAME, Context.MODE_PRIVATE);
         preferences.edit().putString(COOKIE, value).commit();
     }
 
     public static boolean getCustomValue(String customName) {
-        SharedPreferences preferences = RetrofitUtilsApplication.getAppContext().getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        SharedPreferences preferences = RetrofitUtilsApplication.getRetrofitUtilsContext().getSharedPreferences(NAME, Context.MODE_PRIVATE);
         return preferences.getBoolean(customName,false);
     }
     public static void putCustomValue(String customName, boolean customValue) {
-        SharedPreferences preferences = RetrofitUtilsApplication.getAppContext().getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        SharedPreferences preferences = RetrofitUtilsApplication.getRetrofitUtilsContext().getSharedPreferences(NAME, Context.MODE_PRIVATE);
         preferences.edit().putBoolean(customName, customValue).commit();
     }
 
